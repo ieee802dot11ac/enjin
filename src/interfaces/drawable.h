870c663286fce88deb4b5a5d66bd76f3297d88b2
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2024-09-07 22:25:57 -0700

    add Loadable and Drawable interfaces

---
+++ src/interfaces/drawable.h
@@ -0,0 +1,17 @@
+/**
+ * @file drawable.h
+ * @author ieee 802.11ac
+ * @brief Interface for all drawable classes.
+ * @version 0.1
+ * @date 2024-09-07
+ * 
+ */
+
+#pragma once
+
+class Drawable {
+    public:
+    Drawable() {}
+    virtual ~Drawable() {}
+    virtual void Draw();
+};
---
+++ src/interfaces/loadable.h
@@ -0,0 +1,23 @@
+/**
+ * @file loadable.h
+ * @author ieee 802.11ac
+ * @brief Interface for all loadable classes.
+ * @version 0.1
+ * @date 2024-09-07
+ * 
+ */
+
+#pragma once
+
+#include "serial/stream.h"
+#include <string>
+
+class Loadable {
+    public:
+    Loadable() {}
+    virtual ~Loadable() {}
+    virtual void Load(Stream&) = 0;
+    virtual void Save(Stream&) = 0;
+
+    std::string mName;
+};
